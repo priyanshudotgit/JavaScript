46e9cfa8362ebedf1078ca3e0f7c97b5d76ac2d7
Priyanshu Samanta <priyanshusamanta23@gmail.com>
2025-06-11 13:12:35 +0530

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "contactjs@gmail.com"
+var accountPassword = "1234abcd"
+accountCity = "Indore"
+
+// accountId = 2 const cannot be reassigned/changed/modified
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
